293513cf0dc0f528bf698fae68f061340cd87443
PatricioVeliz <velizpatricio02@gmail.com>
2022-06-10 10:14:00 -0300

    navBAR

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h1>PatoBazzar</h1>
+            
+            <button>HOME</button>
+            <button>HOME</button>
+            <button>HOME</button>
+        </nav>
+    )
+}
+export default NavBar 
