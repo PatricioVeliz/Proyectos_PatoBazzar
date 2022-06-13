52b8e8aefba6e5cb733d90a992a45cb0525de19a
PatricioVeliz <velizpatricio02@gmail.com>
2022-06-13 01:53:59 -0300

    probando

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import ItemListContainer from './components/ItemListContainer/ItemListeContainer';
 import NavBar from './components/NavBar/NavBar';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import { useState} from 'react'
+
+
 function App() {
+  const [page, setPage] = useState ('list')
+
+
   return (
     <>
     <NavBar/>
-    <ItemListContainer/>
-    <ItemDetailContainer/>
+    {page ===  'list' && <ItemListContainer/>}
+    {page === 'detail' && <ItemDetailContainer/>}
     </>
   );
 }
