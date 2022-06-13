c86474db42a72d86c7a123a352e8daa693e59bd3
PatricioVeliz <velizpatricio02@gmail.com>
2022-06-13 20:42:02 -0300

    borrar events

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,14 +1,35 @@
-import './ItemDetail'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import ItemCount from '../ItemCount/ItemCount'
+import './ItemDetail.css'
 
-const ItemDetail = ({product}) => {
+
+const ItemDetail = ({ name, description, price, stock,img }) => {
+
+    const [quantity, setQuantity] = useState(0)
+
+    const hanleOnAdd =(count) =>{
+        console.log('agrege al carrito')
+        console.log(count)
+        setQuantity(count)
+    }
+
     return(
         
-        <div>
+        <div className="Container">
             <h1>ItemDetail</h1>
-            <h2>{product}</h2>
+            <div className="Card">
+            <h2>{name}</h2>
+            <img className='imagenes' src={img}></img>
+            <p>{price}</p>
+            <p>{description}</p>
+
+            {quantity > 0 ? <Link to='/Cart'>Finalizar compra</Link> : <ItemCount initial={1} stock={stock} onAdd={hanleOnAdd}/>}
+
+            </div>
         </div>
 
     )
 }
 
-export default ItemDetail
+export default ItemDetail 
